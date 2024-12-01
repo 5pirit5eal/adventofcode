86a287c592341f991e2595b6fb31383277c27769
5prit5eal <rubeneschulze@googlemail.com>
2024-12-01 17:30:19 +0100

    finished first riddle

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,43 @@
+import java.io.File
+import kotlin.math.abs
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val (a, b) = parseInput()
+    println(measureDistance(a, b))
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun parseInput(): Pair<Array<Int>, Array<Int>> {
+    val filePath = "inputs/first_day_input.txt"
+    var a = mutableListOf<Int>()
+    var b = mutableListOf<Int>()
+    // read txt file
+    // iterate and append to output lists
+    File(filePath).useLines { lines ->
+        lines.forEach {
+            val lineList = it.split(" ")
+            a.add(lineList.first().toInt())
+            b.add(lineList.last().toInt())
+        }
     }
+    return Pair(a.toTypedArray(), b.toTypedArray())
+}
+
+
+fun measureDistance(a: Array<Int>, b: Array<Int>): Int {
+    // sort the arrays
+    a.sort()
+    b.sort()
+    if (a.contentEquals(b)) {
+        return 0
+    }
+
+    // loop over array and calculate distance
+    var distance: Int = 0
+    for (i in a.indices) {
+        distance += abs(a[i] - b[i])
+    }
+    // return distance
+    return distance
 }
