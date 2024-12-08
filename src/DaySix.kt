acd5a84afe0c22fd0ae6e97efc61897a424b1934
5prit5eal <rubeneschulze@googlemail.com>
2024-12-08 21:24:34 +0100

    optimized sixth

--- src/DaySix.kt
+++ src/DaySix.kt
@@ -16,6 +16,7 @@ fun sixthDay(): Int {
             matrix[i][j] = lines[i][j]
         }
     }
+    // return getUniquePositions(matrix).distinctBy { Pair(it.first, it.second) }.count()
     return countObstructions(matrix)
 }
 
@@ -38,7 +39,7 @@ enum class Direction(val orientation: Char) {
 
 class Guard(var position: Pair<Int, Int>, var direction: Direction) {
     var inside = true
-    var tries = 0
+    private var tries = 0
 
     init {
         if (position.first < 0 || position.second < 0) {
@@ -52,30 +53,28 @@ class Guard(var position: Pair<Int, Int>, var direction: Direction) {
      * @param matrix the matrix to step through
      * @return true, if the guard stays within the matrix, false otherwise
      */
-    fun nextStep(matrix: Array<CharArray>): Boolean {
+    fun nextStep(matrix: Array<CharArray>) {
         val nextPosition = this.getNextPosition()
         if (isOutside(matrix, nextPosition)) {
             logger.debug { "Outside of matrix at $position, wanting to go to $nextPosition for matrix of [${matrix.size},${matrix[0].size}]" }
-            return false
-        } else if (tries > 5) {
+            inside = false
+        } else if (tries > 4) {
             logger.error { "I am trapped at $position!" }
-            return false
+            inside = false
         } else if (isObstacle(matrix, nextPosition)) {
             logger.debug { "Found an obstacle at $nextPosition, retries at $tries" }
             direction = direction.next()
+            logger.debug { "Moving to $direction" }
             tries++
-            return this.nextStep(matrix)
+            this.nextStep(matrix)
         } else {
             position = nextPosition
             tries = 0
-            return true
         }
     }
 
     private fun isObstacle(matrix: Array<CharArray>, nextPosition: Pair<Int, Int>): Boolean {
-        return (matrix[nextPosition.first][nextPosition.second] in (listOf(
-            '#', 'O'
-        ) + Direction.entries.map { it.orientation }))
+        return (matrix[nextPosition.first][nextPosition.second] in listOf('#', 'O'))
     }
 
     private fun isOutside(matrix: Array<CharArray>, position: Pair<Int, Int>): Boolean {
@@ -83,23 +82,23 @@ class Guard(var position: Pair<Int, Int>, var direction: Direction) {
     }
 
     private fun getNextPosition(): Pair<Int, Int> {
-        return when (this.direction) {
-            Direction.UPWARDS -> Pair(this.position.first - 1, this.position.second)
-            Direction.DOWNWARDS -> Pair(this.position.first + 1, this.position.second)
-            Direction.RIGHT -> Pair(this.position.first, this.position.second + 1)
-            Direction.LEFT -> Pair(this.position.first, this.position.second - 1)
+        return when (direction) {
+            Direction.UPWARDS -> Pair(position.first - 1, position.second)
+            Direction.DOWNWARDS -> Pair(position.first + 1, position.second)
+            Direction.RIGHT -> Pair(position.first, position.second + 1)
+            Direction.LEFT -> Pair(position.first, position.second - 1)
         }
     }
 }
 
-fun getUniquePositions(matrix: Array<CharArray>): Set<Pair<Int, Int>> {
-    val uniquePositions = mutableSetOf<Pair<Int, Int>>()
+fun getUniquePositions(matrix: Array<CharArray>): List<Triple<Int, Int, Direction>> {
+    val uniquePositions = mutableSetOf<Triple<Int, Int, Direction>>()
     var guard: Guard? = null
     outer@ for (i in matrix.indices) {
         for (j in matrix[i].indices) {
             if (matrix[i][j] in Direction.entries.map { it.orientation }) {
                 guard = Guard(Pair(i, j), Direction.fromChar(matrix[i][j])!!)
-                uniquePositions.add(Pair(i, j))
+                uniquePositions.add(Triple(i, j, guard.direction))
                 break@outer
             }
         }
@@ -109,67 +108,44 @@ fun getUniquePositions(matrix: Array<CharArray>): Set<Pair<Int, Int>> {
     }
     logger.info { "Starting guard patrols..." }
     while (guard.inside) {
-        if (!guard.nextStep(matrix)) {
-            guard.inside = false
-        }
-        uniquePositions.add(guard.position)
+        guard.nextStep(matrix)
+        logger.debug { "Adding ${Triple(guard.position.first, guard.position.second, guard.direction)}" }
+        uniquePositions.add(Triple(guard.position.first, guard.position.second, guard.direction))
     }
-    return uniquePositions.toSet()
+    return uniquePositions.toList()
 }
 
 fun countObstructions(matrix: Array<CharArray>): Int {
-    var firstGuard: Guard? = null
-    var startingPosition: Triple<Int, Int, Direction>? = null
-    outer@ for (i in matrix.indices) {
-        for (j in matrix[i].indices) {
-            if (matrix[i][j] in Direction.entries.map { it.orientation }) {
-                firstGuard = Guard(Pair(i, j), Direction.fromChar(matrix[i][j])!!)
-                startingPosition = Triple(i, j, Direction.fromChar(matrix[i][j])!!)
-                break@outer
-            }
-        }
-    }
-    if (firstGuard == null || startingPosition == null) {
-        throw UnexpectedException("Guard should be defined in matrix!")
-    }
     logger.info { "Starting testing obstructions..." }
-    var obstructionCount = 0
-    val obstructionOptions = getUniquePositions(matrix)
-    for (obstruction in obstructionOptions) {
-        val previousObject = matrix[obstruction.first][obstruction.second]
-        if (previousObject in listOf('#', '^')) {
+    val obstructionPositions = mutableSetOf<Pair<Int, Int>>()
+    val obstructionOptions = getUniquePositions(matrix).distinctBy { it.first to it.second }
+    for ((o, obstructionPosition) in obstructionOptions.withIndex()) {
+        if (o < 1) {
             continue
         }
-        matrix[obstruction.first][obstruction.second] = 'O'
-        // init guard
-        val guard = Guard(firstGuard.position, firstGuard.direction)
-        val uniquePositions = mutableSetOf(startingPosition)
+        val previousObject = matrix[obstructionPosition.first][obstructionPosition.second]
+        matrix[obstructionPosition.first][obstructionPosition.second] = 'O'
+        // reinit guard
+        val guard =
+            Guard(obstructionOptions[o - 1].first to obstructionOptions[o - 1].second, obstructionOptions[o - 1].third)
+        val uniquePositions = obstructionOptions.subList(0, o + 1).toMutableSet()
         var obstructionFound = false
 
-        logger.debug { "Starting new run with obstruction at $obstruction" }
+        logger.debug { "Starting new run with guard at ${guard.position} and obstruction at $obstructionPosition" }
         while (guard.inside && !obstructionFound) {
-            val position = guard.position
-            val isInside = guard.nextStep(matrix)
-            if (isInside) {
+            guard.nextStep(matrix)
+            if (guard.inside) {
                 val newlyWalked =
                     uniquePositions.add(Triple(guard.position.first, guard.position.second, guard.direction))
                 if (!newlyWalked) {
-                    logger.debug { "Obstruction found at ${guard.position}" }
+                    logger.debug { "Repetition found at ${guard.position} for obstruction at $obstructionPosition" }
                     obstructionFound = true
-                    obstructionCount++
+                    obstructionPositions.add(Pair(obstructionPosition.first, obstructionPosition.second))
                 }
-            } else {
-                guard.inside = false
             }
-            matrix[position.first][position.second] = '.'
-
         }
         // reset matrix
-        matrix[obstruction.first][obstruction.second] = previousObject
-
+        matrix[obstructionPosition.first][obstructionPosition.second] = previousObject
     }
-
-
-
-    return obstructionCount
+    return obstructionPositions.count()
 }
