80ab99ea0c6ca8ec93b4e373e6e13da0f2671971
5prit5eal <rubeneschulze@googlemail.com>
2024-12-08 09:53:44 +0100

    finished day 5

---
+++ src/DaySix.kt
@@ -0,0 +1,11 @@
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun sixthDay(): Int {
+    val rules = HashMap<String, HashMap<String, MutableList<String>>>()
+    val manuals: MutableList<List<String>> = mutableListOf()
+    val filePath = "inputs/fifth_day_input.txt"
+    // read txt file and construct hashmap and lists
+    return 0
+}
