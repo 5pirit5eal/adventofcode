02d051fda9155d132101e66bd7b7d976c921f986
5prit5eal <rubeneschulze@googlemail.com>
2024-12-12 20:54:27 +0100

    finished day seven part 2

--- src/DaySeven.kt
+++ src/DaySeven.kt
@@ -1,27 +1,39 @@
 import io.github.oshai.kotlinlogging.KotlinLogging
 import java.io.File
+import kotlin.math.log10
+import kotlin.math.pow
 
 private val logger = KotlinLogging.logger {}
 
-fun seventhDay(): Long {
+fun seventhDay(): Pair<Long, Long> {
     val filePath = "inputs/seventh_day_input.txt"
     // read txt file and construct 2D matrix
     val lines = File(filePath).readLines()
-
-    return getOperations(lines)
+    return getBinaryOperations(lines) to getTriOperations(lines)
 }
 
-class Node(val value: Long) {
-    var left: Node? = null
-    var right: Node? = null
+class BinaryNode(val value: Long) {
+    var left: BinaryNode? = null
+    var right: BinaryNode? = null
 
     override fun toString(): String {
         return "$value"
     }
 }
 
+class TriNode(val value: Long) {
+    var left: TriNode? = null
+    var middle: TriNode? = null
+    var right: TriNode? = null
+
+    override fun toString(): String {
+        return "$value"
+    }
+}
+
+
 class BinaryTree(operands: List<Long>) {
-    private val root: Node = Node(operands[0])
+    private val root: BinaryNode = BinaryNode(operands[0])
 
     init {
         for (operand in operands.drop(1)) {
@@ -29,16 +41,16 @@ class BinaryTree(operands: List<Long>) {
         }
     }
 
-    private fun addNode(operand: Long, node: Node) {
-        if (node.left == null) {
-            node.left = Node(node.value * operand)
+    private fun addNode(operand: Long, binaryNode: BinaryNode) {
+        if (binaryNode.left == null) {
+            binaryNode.left = BinaryNode(binaryNode.value * operand)
         } else {
-            this.addNode(operand, node.left!!)
+            this.addNode(operand, binaryNode.left!!)
         }
-        if (node.right == null) {
-            node.right = Node(node.value + operand)
+        if (binaryNode.right == null) {
+            binaryNode.right = BinaryNode(binaryNode.value + operand)
         } else {
-            this.addNode(operand, node.right!!)
+            this.addNode(operand, binaryNode.right!!)
         }
     }
 
@@ -46,27 +58,78 @@ class BinaryTree(operands: List<Long>) {
         return this.findResultNode(equationResult, this.root)
     }
 
-    private fun findResultNode(equationResult: Long, node: Node?): Long {
-        if (node!!.left == null || node.right == null) {
-            if (equationResult == node.value) {
+    private fun findResultNode(equationResult: Long, binaryNode: BinaryNode?): Long {
+        if (binaryNode!!.left == null || binaryNode.right == null) {
+            if (equationResult == binaryNode.value) {
                 return equationResult
             } else {
                 return 0
             }
         } else {
-            logger.debug { "Searching for $equationResult, Looking at $node - left: ${node.left}, right: ${node.right}" }
-            if (equationResult == this.findResultNode(equationResult, node.left)) {
-                return equationResult
-            } else if (equationResult == this.findResultNode(equationResult, node.right)) {
-                return equationResult
+            logger.debug { "Searching for $equationResult, Looking at $binaryNode - left: ${binaryNode.left}, right: ${binaryNode.right}" }
+            return when (equationResult) {
+                this.findResultNode(equationResult, binaryNode.left) -> equationResult
+                this.findResultNode(equationResult, binaryNode.right) -> equationResult
+                else -> 0
             }
-            return 0
         }
 
     }
 }
 
-private fun getOperations(equations: List<String>): Long {
+class TriTree(operands: List<Long>) {
+    private val root: TriNode = TriNode(operands[0])
+
+    init {
+        for (operand in operands.drop(1)) {
+            this.addNode(operand, this.root)
+        }
+    }
+
+    private fun addNode(operand: Long, triNode: TriNode) {
+        if (triNode.left == null) {
+            triNode.left = TriNode(triNode.value * operand)
+        } else {
+            this.addNode(operand, triNode.left!!)
+        }
+        if (triNode.right == null) {
+            triNode.right = TriNode(triNode.value + operand)
+        } else {
+            this.addNode(operand, triNode.right!!)
+        }
+        if (triNode.middle == null) {
+            val magnitude = 10.0.pow(log10(operand.toDouble()).toInt() + 1).toLong()
+            triNode.middle = TriNode(triNode.value * magnitude + operand)
+        } else {
+            this.addNode(operand, triNode.middle!!)
+        }
+    }
+
+    fun findResult(equationResult: Long): Long {
+        return this.findResultNode(equationResult, this.root)
+    }
+
+    private fun findResultNode(equationResult: Long, triNode: TriNode?): Long {
+        if (triNode!!.left == null || triNode.right == null || triNode.middle == null) {
+            if (equationResult == triNode.value) {
+                return equationResult
+            } else {
+                return 0
+            }
+        } else {
+            logger.debug { "Searching for $equationResult, Looking at $triNode - left: ${triNode.left}, middle: ${triNode.middle}, right: ${triNode.right}" }
+            return when (equationResult) {
+                this.findResultNode(equationResult, triNode.left) -> equationResult
+                this.findResultNode(equationResult, triNode.middle) -> equationResult
+                this.findResultNode(equationResult, triNode.right) -> equationResult
+                else -> 0
+            }
+        }
+
+    }
+}
+
+private fun getBinaryOperations(equations: List<String>): Long {
     // split by :
     var result: Long = 0
     for (equation in equations) {
@@ -74,6 +137,22 @@ private fun getOperations(equations: List<String>): Long {
         val equationResult = rawEquationResult.toLong()
         val operands = rawOperands.split(" ").drop(1).map { it -> it.toLong() }
         val tree = BinaryTree(operands)
+        val searchResult = tree.findResult(equationResult)
+        logger.info { "Found $searchResult, expected $equationResult" }
+        result += searchResult
+
+    }
+    return result
+}
+
+private fun getTriOperations(equations: List<String>): Long {
+    // split by :
+    var result: Long = 0
+    for (equation in equations) {
+        val (rawEquationResult, rawOperands) = equation.split(":")
+        val equationResult = rawEquationResult.toLong()
+        val operands = rawOperands.split(" ").drop(1).map { it -> it.toLong() }
+        val tree = TriTree(operands)
         val searchResult = tree.findResult(equationResult)
         logger.info { "Found $searchResult, expected $equationResult" }
         result += searchResult
